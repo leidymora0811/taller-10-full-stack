be9a9420f6a72680b7d465b4e5e1f5174dbabe6c
leidymora0811 <moraperezleidy@gmail.com>
2023-11-21 11:06:10 -0500

    Punto 1 realizado

---
+++ punto-1/index.php
@@ -0,0 +1,14 @@
+<?php
+//DATOS DE ENTRADA
+
+$name = "Geovanny";
+const WELCOME = "Hola bienvenido";
+
+//PROCESO
+echo WELCOME . " ";
+echo $name . "\n";
+$name = "Bruno";
+echo WELCOME . " ";
+echo $name;
+
+?>
